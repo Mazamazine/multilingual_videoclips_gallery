82e1e27794f93e2bc7b139c1afc67ec166afa424
Nina <nina@koumbit.org>
2021-12-17 14:41:26 -0500

    Create/set parent languages catergory

--- index.php
+++ index.php
@@ -5,8 +5,18 @@ Description: Adds ability to include a video gallery in a post and choose langua
 Author: Nina Ripoll
 */
 
+$vcg_language_catId = false;
+
 function vcg_plugin_init() {
+  // Load translations
   load_plugin_textdomain( 'vcg', false, 'capsules_videos/languages' );
+  // We need the 'vcg_languages' categorie for medias languages
+  // Create it if not present
+  $newTerm = wp_insert_term( 'vcg_languages', 'category' );
+  if(is_array($newTerm)) $parentCat = $newTerm['term_id'];
+  else $parentCat = $newTerm->error_data['term_exists'];
+  global $vcg_language_catId;
+  $vcg_language_catId = $parentCat;
 }
 add_action('init', 'vcg_plugin_init');
 
